6826b4275436fcfd154794609dd0a300172835ab
Your Name <you@example.com>
2019-04-30 15:53:10 +0800

    首页数据渲染完毕

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/style.css'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import VueRouter from 'vue-router';
+Vue.use( VueRouter);
+
+import index from './components/index.vue'
+const routes=[
+  {path:'/index',component:index}
+]
+
+const router=new VueRouter({routes});
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
